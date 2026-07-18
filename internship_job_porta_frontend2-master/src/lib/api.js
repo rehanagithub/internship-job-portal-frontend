c881d6e2a6f51e96e306a4704191dc4115b94cea
Rehana <shaikrehana430@gmail.com>
2026-07-18 14:47:48 +0530

    commit changes

--- internship_job_porta_frontend2-master/src/lib/api.js
+++ internship_job_porta_frontend2-master/src/lib/api.js
@@ -1,9 +1,11 @@
 const BASE_URL = import.meta.env.BASE_URL?.replace(/\/$/, '') || '';
 // export const API_BASE = `http://localhost:5000/api`;
 //export const API_BASE = `https://internship-job-portal-app-baackend.onrender.com`;
-// export const API_BASE = `https://internship-job-porta-backend.onrender.com`;
+export const API_BASE = `https://internship-job-porta-backend.onrender.com`;
 
-export const API_BASE = `https://internship-job-porta-backend.onrender.com/`;
+
+
+
 
 async function request(path, options = {}) {
   const token = localStorage.getItem('access_token');
